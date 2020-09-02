113b096fb6ce1c2d7c3e8ed475a3b3e2f06d1034
Kanak Tyagi <tyagiknk.india@gmail.com>
2020-09-02 19:23:41 +0530

    Added the dummy feed to be passed as a props to Post component

--- src/Feed.js
+++ src/Feed.js
@@ -2,12 +2,23 @@ import React from 'react'
 import './Feed.css'
 import StoryReel from './StoryReel'
 import MessageSender from './MessageSender'
+import Post from './Post'
+import imagek from './image/my_dp.jpg'
 
 function Feed() {
     return (
         <div className='feed'>
             <StoryReel/>
             <MessageSender />
+            <Post 
+                profilePic={imagek}
+                message="Wow this is works!"
+                timestamp="This is a timestamp"
+                userName="kanak tyagi"
+                image="https://code.org/shared/images/social-media/codeorg2019_social.png"
+            />
+            <Post />
+            <Post />
         </div>
     )
 }
