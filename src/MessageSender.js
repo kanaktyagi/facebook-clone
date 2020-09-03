29b696705ac9631f1868273bd1226ba37c33c18e
Kanak Tyagi <tyagiknk.india@gmail.com>
2020-09-03 12:43:20 +0530

    Added the user display pic and name from useState

--- src/MessageSender.js
+++ src/MessageSender.js
@@ -4,10 +4,12 @@ import { Avatar } from '@material-ui/core'
 import VideocamIcon from '@material-ui/icons/Videocam';
 import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary';
 import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import { useStateValue } from './StateProvider';
 
 function MessageSender() {
     const [input, setInput] = useState("");
     const [imageUrl, setImageUrl] = useState("");
+    const [{user}, dispatch] = useStateValue();
 
     const handleSumbit = (e) => {
         e.preventDefault();
@@ -17,13 +19,13 @@ function MessageSender() {
     return (
         <div className="messageSender">
             <div className="messageSender__top">
-                <Avatar />
+                <Avatar src={user.photoURL}/>
                 <form>
                     <input
                     value={input}
                     onChange={(e) => setInput(e.target.value)}
                     className="messageSender__input"
-                    placeholder={`what's on your mind ?`}
+                    placeholder={`what's on your mind ${user.displayName}?`}
                     />
                     <input 
                     value={imageUrl}
