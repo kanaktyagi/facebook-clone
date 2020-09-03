4c247a0726b03fd6f5491ad383f6d59fd22eaf05
Kanak Tyagi <tyagiknk.india@gmail.com>
2020-09-03 13:58:13 +0530

    Added the functionality to add data realtime and save it to the firebase

--- src/MessageSender.js
+++ src/MessageSender.js
@@ -5,6 +5,8 @@ import VideocamIcon from '@material-ui/icons/Videocam';
 import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary';
 import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 import { useStateValue } from './StateProvider';
+import db from './firebase'
+import firebase from "firebase"
 
 function MessageSender() {
     const [input, setInput] = useState("");
@@ -13,6 +15,13 @@ function MessageSender() {
 
     const handleSumbit = (e) => {
         e.preventDefault();
+        db.collection('posts').add({
+            message: input,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            profiePic: user.photoURL,
+            userName: user.displayName,
+            image:imageUrl
+        })
         setInput("");
         setImageUrl("");
     }
