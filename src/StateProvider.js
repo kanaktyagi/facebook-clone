da0b82d576f8d5fc720643047c40dad943bf7cbf
Kanak Tyagi <tyagiknk.india@gmail.com>
2020-09-03 12:26:58 +0530

    Added the contextAPI to the project

---
+++ src/StateProvider.js
@@ -0,0 +1,17 @@
+// set the data layer
+// we need to track the basket
+
+import React, { createContext, useContext, useReducer } from "react";
+
+// This is the data layer
+export const StateContext = createContext();
+
+//Build a provider
+export const StateProvider = ({reducer, initialState, children}) => (
+ <StateContext.Provider value={useReducer(reducer, initialState)}>
+ {children}
+ </StateContext.Provider>
+)  
+
+// to use it inside of a component
+export const useStateValue= () => useContext(StateContext)
