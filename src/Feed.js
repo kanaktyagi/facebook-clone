af80b5af75172299cb10125d994899dc9a1a9491
Kanak Tyagi <tyagiknk.india@gmail.com>
2020-09-03 13:45:22 +0530

    Connected to the firebase database to display feed from there

--- src/Feed.js
+++ src/Feed.js
@@ -1,24 +1,39 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import './Feed.css'
 import StoryReel from './StoryReel'
 import MessageSender from './MessageSender'
 import Post from './Post'
 import imagek from './image/my_dp.jpg'
+import db from './firebase'
 
 function Feed() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+       db.collection("posts").orderBy('timestamp','desc').onSnapshot(snapshot =>(
+          setPosts(snapshot.docs.map(doc =>({
+              id: doc.id,
+              data: doc.data()
+          })
+          ))
+       ))
+    }, [])
+
     return (
         <div className='feed'>
             <StoryReel/>
             <MessageSender />
-            <Post 
-                profilePic={imagek}
-                message="Wow this is works!"
-                timestamp="This is a timestamp"
-                userName="kanak tyagi"
-                image="https://code.org/shared/images/social-media/codeorg2019_social.png"
-            />
-            <Post />
-            <Post />
+           {posts.map(post=> (
+               <Post
+                key={post.id}
+                profilePic={post.data.profilePic}
+                message={post.data.message}
+                timestamp={post.data.timestamp}
+                userName={post.data.userName}
+                image={post.data.image}
+               />
+           )
+           )}
         </div>
     )
 }
